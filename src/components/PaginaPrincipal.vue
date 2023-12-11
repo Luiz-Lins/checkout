78506f714a0003971683584324bab08e5afb6a96
luiz <luizlins22@gmail.com>
2023-12-11 02:15:22 -0300

    Adicionado validação de campos

--- src/components/PaginaPrincipal.vue
+++ src/components/PaginaPrincipal.vue
@@ -1,77 +1,66 @@
 <template>
-  <div class="row justify-content-around">
-    <div class="col-12">
-      <div class="input-group mb-3">
-        <label class="input-group-text" for="inputGroupSelect01">Adultos</label>
-        <select class="form-control" id="inputGroupSelect01">
-          <option selected>Seleciona a quantidade</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
+  <form class="row g-3">
+    <div class="input-group input-daterange">
+        <input type="text" placeholder="data de ida" class="form-control" value="" required>
+        <div class="input-group-addon">to</div>
+        <input type="text" placeholder="data de volta" class="form-control" value="" required>
+    </div>
+    <div class="col-md-6">
+      <div class="input-group">
+        <input type="text" placeholder="Adultos" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2">
+          <select class="form-control" id="inputGroupSelect01">
+            <option selected>Seleciona a quantidade</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          required </select>
       </div>
-      <div class="input-group mb-3">
-        <label class="input-group-text" for="inputGroupSelect01">Crianças</label>
-        <select class="form-select" id="inputGroupSelect01">
-          <option selected>Seleciona a quantidade</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
+    </div>
+    <div class="col-md-6">
+      <div class="input-group">
+        <input type="text" placeholder="Crianças" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2">
+          <select class="form-control" id="inputGroupSelect01">
+            <option selected>Seleciona a quantidade</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
       </div>
+    </div>
+    <div class="col-md-12">
       <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
         <option selected>Selecione a origem</option>
         <option v-for="destino in destinos" :value="destino.id" :key="destino.id">
           {{ destino.cidade }}
         </option>
-      </select>
+      required </select>
       <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
         <option selected>Escolha um destino</option>
         <option v-for="destino in destinos" :value="destino.id" :key="destino.id">
           {{ destino.cidade }}
         </option>
       </select>
-
-      <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">Nome</span>
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Principal passageiro"
-          aria-label="Username"
-          aria-describedby="basic-addon1"
-        />
-      </div>
-
-      <div class="input-group mb-3">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Email do principal passageiro"
-          aria-label="Recipient's username"
-          aria-describedby="basic-addon2"
-        />
-        <span class="input-group-text" id="basic-addon2">@example.com</span>
-      </div>
-
-      <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-        <label class="form-check-label" for="flexSwitchCheckDefault">Ida e volta</label>
-      </div>
-      <div class="input-group input-daterange">
-          <input type="text" class="form-control" value="2012-04-05">
-          <div class="input-group-addon">to</div>
-          <input type="text" class="form-control" value="2012-04-19">
-      </div>
-      <div class="col-12">
-        <button class="btn btn-primary" type="submit">Enviar Checkout</button>
+    </div>
+    <div class="col-md-12">
+      <div class="input-group">
+        <input type="text" placeholder="Nome do principal passageiro" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required>
       </div>
     </div>
-  </div>
+    <div class="col-md-12">
+      <div class="input-group">
+        <input type="text" placeholder="Email do principal passageiro" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required>
+      </div>
+    </div>
+    <div class="col-12">
+      <div id="liveAlertPlaceholder">
+        <button class="btn btn-primary" id="liveAlertBtn" type="submit">Enviar Checkout</button>
+      </div>
+    </div>
+  </form>
 </template>
 
 <style>
@@ -103,3 +92,5 @@ export default {
   }
 }
 </script>
+
+
